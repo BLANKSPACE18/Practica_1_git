b6681dc1a285b5511909ab6ebd1e5bae43648c36
poncho <jalonso.avila18@gmail.com>
2025-09-10 12:46:35 -0600

    quite texto

--- app/src/main/java/com/example/practica_1_git/MainActivity.kt
+++ app/src/main/java/com/example/practica_1_git/MainActivity.kt
@@ -36,10 +36,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!", // pasar el nombre de parametro a la funcion
         modifier = modifier
     )
-    Text(
-        text = "Hello $name!", // pasar el nombre de parametro a la funcion
-        modifier = modifier
-    )
+
 
 
 }
